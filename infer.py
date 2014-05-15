eb58da0fc84e0454fce997074e26c009cd8f0014
Pranav Ravichandran <me@onloop.net>
2014-05-15 12:45:54 +0530

    Overhauled class structure to support better persistence

--- infer.py
+++ infer.py
@@ -1,21 +1,46 @@
 #!/usr/bin/env python
 
+''' Wrapper for Gensim LSI Utilities.
+    Pranav Ravichandran <me@onloop.net> '''
+
 from gensim import corpora, models, similarities
 from itertools import chain
 import logging
+import os, errno
 
+# Setup basic logging.
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 class Infer:
-    def __init__(self, path_to_dict=None, path_to_corpus=None, path_to_index=None, path_to_model=None):
-        self.path_to_dict = path_to_dict or '/tmp/infer.dict'
-        self.path_to_corpus = path_to_corpus or '/tmp/infer.mm'
-        self.path_to_index = path_to_index or '/tmp/infer.index'
-        self.path_to_model = path_to_model or '/tmp/infer.model'
-        self.dictionary = self.corpus = self.lsi_model = self.index = None
-        self.existing_corpus = self.documents = None
+    def __init__(self, path=None):
+        path = path or 'datasets'
+        self.__setup_path(path)
 
-    def build(self, documents, stopwords=[]):
+        self.path_to_dict =  os.path.join(path, 'infer.dict')
+        self.path_to_corpus = os.path.join(path, 'infer.mm')
+        self.path_to_index =  os.path.join(path, 'infer.index')
+        self.path_to_model = os.path.join(path, 'infer.model')
+
+        self.documents = None
+        self.dictionary = self.__load_from_fname(lambda: corpora.Dictionary.load(self.path_to_dict))
+        self.corpus = self.__load_from_fname(lambda: corpora.MmCorpus(self.path_to_corpus))
+        self.lsi_model = self.__load_from_fname(lambda: models.LsiModel.load(self.path_to_model))
+        self.index = self.__load_from_fname(lambda: similarities.Similarity.load(self.path_to_index))
+
+    def __load_from_fname(self, read_func):
+        try:
+            return read_func()
+        except IOError:
+            return None
+
+    def __setup_path(self, path):
+        try:
+            os.makedirs(path)
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise
+
+    def build(self, documents, stopwords=[], update=True, num_topics=2, num_features=500):
         self.documents = documents
         texts = [[word for word in document.lower().split() if word not in stopwords] for document in self.documents]
         all_tokens = sum(texts, [])
@@ -23,42 +48,35 @@ class Infer:
         tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
         texts = [[word for word in text if word not in tokens_once] for text in texts]
 
-        try:
-            self.dictionary = corpora.Dictionary.load(self.path_to_dict)
-        except IOError:
+        if update and self.dictionary:
+            self.dictionary.add_documents(texts)
+        else:
             self.dictionary = corpora.Dictionary(texts)
 
-        try:
-            self.existing_corpus = corpora.MmCorpus(self.path_to_corpus)
-        except IOError:
-            self.existing_corpus = []
+        self.dictionary.save(self.path_to_dict)
 
         corpus = [self.dictionary.doc2bow(text, allow_update=True) for text in texts]
+        corpora.MmCorpus.serialize(self.path_to_corpus, chain(corpus, self.corpus if update and self.corpus else []))
 
-        corpora.MmCorpus.serialize(self.path_to_corpus, chain(corpus, self.existing_corpus))
-        self.dictionary.save(self.path_to_dict)
         self.corpus = corpora.MmCorpus(self.path_to_corpus)
 
-    def infer(self, query, num_topics=2):
-        try:
-            self.lsi_model = models.LsiModel.load(self.path_to_model)
+        if update and self.lsi_model:
             self.lsi_model.add_documents(self.corpus)
-        except IOError:
-            try:
-                self.lsi_model = models.LsiModel(self.corpus, id2word=self.dictionary, num_topics=num_topics)
-            except ValueError:
-                return []
+        else:
+            self.lsi_model = models.LsiModel(self.corpus, id2word=self.dictionary, num_topics=num_topics)
 
         self.lsi_model.save(self.path_to_model)
 
+        if update and self.index:
+            self.index.add_documents(self.corpus)
+        else:
+            self.index = similarities.Similarity(self.path_to_index, self.lsi_model[self.corpus], num_features)
+
+        self.index.save(self.path_to_index)
+
+    def infer(self, query):
         vec_bow = self.dictionary.doc2bow(query.lower().split())
         vec_lsi = self.lsi_model[vec_bow]
-
-        try:
-            self.index = similarities.Similarity.load(self.path_to_index)
-            self.index.add_documents(self.corpus)
-        except IOError:
-            self.index = similarities.Similarity(self.path_to_index, self.lsi_model[self.corpus], 500)
 
         sims = self.index[vec_lsi]
         sims = sorted(enumerate(sims), key=lambda item: -item[1])
